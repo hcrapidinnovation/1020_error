cd91557a02f312685980f20af06cb059453db443
dc-cz2p <dc@rapidinnovation.dev>
2022-09-09 12:58:30 +0530

    api update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let CountryName;
 
-const getCountryName = async (ipAddress) => {
-  const response = await fetch(`https://api.ip2location.com/v2/?ip=${ipAddress}&addon=country&key=JATBCTCZA2`);
+const getCountryName = async () => {
+  const response = await fetch(`https://ipapi.co/json/`);
   const data = await response.json();
+  console.log("====================================");
+  console.log(data);
+  console.log("====================================");
   return data;
 };
 
@@ -12,17 +15,17 @@ const getIp = async () => {
   return data.ip;
 };
 
-getIp().then((ipAddress) => {
-  getCountryName(ipAddress)
-    .then((countryData) => {
-      CountryName = countryData.country.name;
-      document.getElementById(
-        "country-msg"
-      ).innerText = `We can't accept players from ${CountryName}. Do not hesitate to contact us if you need assistance. If you're traveling, you can still use a VPN.`;
-    })
-    .catch(() => {
-      document.getElementById("country-msg").innerText = `We can't accept players from your COUNTRY. Do not hesitate to contact us if 
+// getIp().then((ipAddress) => {
+getCountryName()
+  .then((countryData) => {
+    CountryName = countryData.country_name;
+    document.getElementById(
+      "country-msg"
+    ).innerText = `We can't accept players from ${CountryName}. Do not hesitate to contact us if you need assistance. If you're traveling, you can still use a VPN.`;
+  })
+  .catch(() => {
+    document.getElementById("country-msg").innerText = `We can't accept players from your COUNTRY. Do not hesitate to contact us if 
     you need assistance. If you're travelling, you can still use a VPN.
 `;
-    });
-});
+  });
+// });
